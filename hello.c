e338c62c9a103c8b5b08264fa1f53e9a8def322b
odalys garcia <ogarcia2@huskers.unl.edu>
2020-08-20 07:01:23 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Odalys Garcia
+ *         ogarcia2@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Odalys Garcia\n");
+
+  return 0;
+}
